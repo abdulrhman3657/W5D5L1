c504fbc4f0efead5649fd44864f186dab948d67b
user1 <user01@em/com>
2025-05-15 16:00:45 +0300

    c2

--- main.js
+++ main.js
@@ -16,15 +16,14 @@ const app =
 
     <div className="container p-3">
         <h1>Abdulrhman Alnafisi</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, officiis perspiciatis dignissimos nam voluptatibus consequatur repudiandae facere numquam ipsam qui eum cum. Libero commodi totam inventore voluptatibus adipisci doloremque iure.</p>
+        <p>Full stack developer. Passionate about learning and building reliable web applications, with a strong understanding of JavaScript, HTML and CSS</p>
 
         <div>
             <h3 className="red">Work Experience</h3>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, officiis perspiciatis dignissimos nam voluptatibus consequatur repudiandae facere numquam ipsam qui eum cum. Libero commodi totam inventore voluptatibus adipisci doloremque iure.</p>
+            <h3>Javascript bootcamp</h3>
             <ul>
-                <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, officiis perspiciatis dignissimos nam</li>
-                <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, officiis perspiciatis dignissimos nam</li>
-                <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, officiis perspiciatis dignissimos nam</li>
+                <li>Actively learning HTML, CSS, JavaScript, and Git/GitHub fundamentals</li>
+                <li>Collaborated with a team to develop user-friendly websites</li>
             </ul>   
         </div>
 
@@ -33,25 +32,25 @@ const app =
             <div>
                 <h3><b>Full-Stack App with Authentication</b></h3>
                 <ul>
-                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, officiis perspiciatis dignissimos nam</li>
-                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, officiis perspiciatis dignissimos nam</li>
-                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, officiis perspiciatis dignissimos nam</li>
+                    <li>Developed a full-stack web app using Node.js, Express, and MongoDB for the backend and React, Redux for the frontend.</li>
+                    <li>Implemented JWT authentication for secure user login, signup, and token-based authentication.</li>
+                    <li>Enabled users to add and delete goals with real-time state management using Redux.</li>
                 </ul> 
             </div>
             <div>
-                <h3><b>Designed a website using Boostrap</b></h3>
+                <h3><b>YouTube Video Data Extractor App</b></h3>
                 <ul>
-                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, officiis perspiciatis dignissimos nam</li>
-                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, officiis perspiciatis dignissimos nam</li>
-                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, officiis perspiciatis dignissimos nam</li>
+                    <li>Built a website using Node.js, Express for the backend and ejs and Bootstrap for the front end.</li>
+                    <li>The user provides a YouTube video link, and the website returns the video data including title, view count, likes, description and thumbnail</li>
+                    <li>Deployed the application using render and managed the app using GitHub.</li>
                 </ul> 
             </div>
                         <div>
-                <h3><b>Designed a website using Boostrap</b></h3>
+                <h3><b>EduBot: University Policy Chatbot Assistant</b></h3>
                 <ul>
-                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, officiis perspiciatis dignissimos nam</li>
-                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, officiis perspiciatis dignissimos nam</li>
-                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, officiis perspiciatis dignissimos nam</li>
+                    <li>Developed an AI chatbot LLM with RAG to help students in navigating Qassim University policies in Arabic.</li>
+                    <li>Integrated an embedding database for semantic search, enhancing response and relevance.</li>
+                    <li>Collected and processed with a team a dataset from Saudi universities' rules and regulations.</li>
                 </ul> 
             </div>   
         </div>
@@ -65,9 +64,9 @@ const app =
         <div>
             <h3 className="red">Skills</h3>
                 <ul>
-                    <li>task1</li>
-                    <li>task2</li>
-                    <li>task3</li>
+                    <li>Python, JavaScript, HTML, CSS</li>
+                    <li>git/github</li>
+                    <li>REST APIs</li>
                 </ul>
         </div>  
 
